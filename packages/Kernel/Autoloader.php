4e91ed434a19eed792ce3308389036b7f0232080
Derrick <dantaya90@gmail.com>
2017-04-05 08:57:34 -0400

    Added function to replace the first occurance of a string inside of a string. Modified the autoloader to replace namespace paths of core files to lowercase.

--- packages/Kernel/Autoloader.php
+++ packages/Kernel/Autoloader.php
@@ -6,12 +6,34 @@
  * @return void
  */
 spl_autoload_register(function ($class) {
-	// Replace the \ with / in the class name and add the file extension to generate full file path
-
+	// replace \ with / and add the extenstion
 	$file = ROOT_DIR . str_replace('\\', '/', $class) . '.php';
+
+	// replace core file system folders with lower case names
+	$file = str_replace_first('/Packages/', '/packages/', $file);
+	$file = str_replace_first('/App/', '/app/', $file);
+	
 
 	// if the file exists, require it
 	if (file_exists($file)) {
 		require $file;
 	}
 });
+
+/**
+ * Replace the first occurance of a substring with a new string
+ * 
+ *    @TODO: Possibly move this to a different location
+ * 
+ * @param string $find
+ * @param string $replace
+ * @param string $string
+ */
+function str_replace_first($find, $replace, $string) {
+	$pos = strpos($string, $find);
+	if ($pos !== false) {
+		return substr_replace($string, $replace, $pos, strlen($find));
+	}
+	return $string;
+}
+	
