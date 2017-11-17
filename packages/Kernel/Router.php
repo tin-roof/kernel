96d859f223e296989831ba71a0012fcfd132a9ff
dantaya <dantaya90@gmail.com>
2017-11-17 14:29:16 -0500

    Fixing test

--- packages/Kernel/Router.php
+++ packages/Kernel/Router.php
@@ -135,7 +135,6 @@ class Router
 	 * Define the variables from the route
 	 *
 	 * @param string $request request uri
-	 * 
 	 * @param array $routeVariables variables that are defined in the Route
 	 * @return array
 	 */
